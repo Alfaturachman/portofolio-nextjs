a3bd016fe3c9039caf727c60d26ead203a67d3ec
alfaturachman <122202202929@mhs.dinus.ac.id>
2026-05-30 12:47:07 +0700

    fix cache

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    headers: async () => [
+        {
+            source: '/(.*)',
+            headers: [
+                {
+                    key: 'Cache-Control',
+                    value: 'no-store, must-revalidate',
+                },
+            ],
+        },
+        {
+            source: '/assets/(.*)',
+            headers: [
+                {
+                    key: 'Cache-Control',
+                    value: 'public, max-age=31536000, immutable',
+                },
+            ],
+        },
+    ],
 };
 
 export default nextConfig;

d4d73257226e74003693461dd28e08e474c95e95
alfaturachman <122202202929@mhs.dinus.ac.id>
2026-08-29 22:18:54 +0700

    feat: change image on about page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -107,7 +107,7 @@ export default function RootLayout({
                             '@type': 'Person',
                             name: 'Alfaturachman Maulana Pahlevi',
                             url: 'https://almavi.vercel.app/',
-                            image: 'https://almavi.vercel.app/assets/images/profile/profile.jpg',
+                            image: 'https://almavi.vercel.app/assets/images/profile/profile_almavi.png',
                             sameAs: [
                                 'https://github.com/Alfaturachman',
                                 'https://www.linkedin.com/in/alfaturachman-maulana-pahlevi-4981302b6/',

2746c9176a18fed79c22515696f6f50140995fdf
alfaturachman <122202202929@mhs.dinus.ac.id>
2026-09-02 05:58:06 +0700

    style(navbar): hide Skills nav link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -97,7 +97,7 @@ export default function Navbar() {
         // { label: t.navbar.home, href: '/' },
         // { label: t.navbar.about, href: '/#about' },
         { label: t.navbar.portfolio, href: '/portfolio' },
-        { label: t.navbar.skills, href: '/skills' },
+        // { label: t.navbar.skills, href: '/skills' },
         { label: t.navbar.experience, href: '/#experience' },
         { label: t.navbar.certificates, href: '/certificates' },
     ];
